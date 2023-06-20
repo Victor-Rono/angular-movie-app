337566856a3bb76e28d57bc7e02e07167ef9b31d
Kepha Kibet <kibet.kepha@live.com>
2023-06-20 15:12:20 +0300

    responsive login page

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
